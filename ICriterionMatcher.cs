15a9b930ac78072c0e3959dae7ac4cf2390ac294
Константин Дьяченко <konard@me.com>
2019-08-01 20:06:46 +0300

    Class renamed. Comments added.

--- ICriterionMatcher.cs
+++ ICriterionMatcher.cs
@@ -1,7 +1,18 @@
 ﻿namespace Platform.Interfaces
 {
-    public interface ICreteriaMatcher<TArgument>
+    /// <summary>
+    /// Defines a criterion matcher, that contains a specific method of determining whether the argument matches criterion or not.
+    /// Определяет объект который проверяет соответствие критерию и содержащий конкретный метод определения, соответствует ли аргумент критерию или нет.
+    /// </summary>
+    /// <typeparam name="TArgument">Argument type. Тип аргумента.</typeparam>
+    public interface ICriterionMatcher<TArgument>
     {
+        /// <summary>
+        /// Determines whether the argument matches the criterion.
+        /// Определяет, соответствует ли аргумент критерию.
+        /// </summary>
+        /// <param name="argument">The argument. Аргумент.</param>
+        /// <returns>A value that determines whether the argument matches the criterion. Значение, определяющие соответствует ли аргумент критерию.</returns>
         bool IsMatched(TArgument argument);
     }
 }
