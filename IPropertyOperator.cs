500cf1bcff6e8fbed72b01a4954177a5b83ef8d9
Константин Дьяченко <konard@me.com>
2019-08-01 20:11:42 +0300

    ISpecificPropertyOperator interface renamed to IPropertyOperator.

--- IPropertyOperator.cs
+++ IPropertyOperator.cs
@@ -1,7 +1,19 @@
 ﻿namespace Platform.Interfaces
 {
-    public interface ISpecificPropertyOperator<TObject, TValue> : IProvider<TValue, TObject>
+    /// <summary>
+    /// Defines a specific property operator that is able to get or set values of that property.
+    /// Определяет оператор определённого свойства, который может получать или установливать его значения.
+    /// </summary>
+    /// <typeparam name="TObject">Object type. Тип объекта.</typeparam>
+    /// <typeparam name="TValue">Property value type. Тип значения свойства.</typeparam>
+    public interface IPropertyOperator<TObject, TValue> : IProvider<TValue, TObject>
     {
+        /// <summary>
+        /// Sets the value of a specific property in the specified object.
+        /// Устанавливает значение определённого свойства в указанном объекте.
+        /// </summary>
+        /// <param name="object">The object reference. Ссылка на объект.</param>
+        /// <param name="value">The value. Значение.</param>
         void Set(TObject @object, TValue value);
     }
 }
