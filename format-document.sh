225321adf0f16ff84e48b29b1fb2cd23a86dbb90
Константин Дьяченко <konard@me.com>
2019-08-25 13:10:13 +0300

    Fix format document.

--- format-document.sh
+++ format-document.sh
@@ -5,9 +5,9 @@ set -e # Exit with nonzero exit code if anything fails
 wget https://raw.githubusercontent.com/gpoore/fvextra/cc1c0c5f7b92023cfec67084e2a87bdac520414c/fvextra/fvextra.sty
 
 printf """
-\\documentclass[11pt,a4paper,fleqn]{report}
+\\\documentclass[11pt,a4paper,fleqn]{report}
 \\\usepackage[left=5mm,top=5mm,right=5mm,bottom=5mm]{geometry}
-\\textwidth=200mm
+\\\textwidth=200mm
 \\\usepackage[utf8]{inputenc}
 \\\usepackage[T1]{fontenc}
 \\\usepackage[T2A]{fontenc}
@@ -16,25 +16,25 @@ printf """
 \\\usemintedstyle{vs}
 \\\usepackage{makeidx}
 \\\usepackage[columns=1]{idxlayout}
-\\makeindex
-\\renewcommand{\\thesection}{\\arabic{chapter}.\\arabic{section}}
-\\setcounter{chapter}{1}
-\\setcounter{section}{0}
+\\\makeindex
+\\\renewcommand{\\\thesection}{\\\arabic{chapter}.\\\arabic{section}}
+\\\setcounter{chapter}{1}
+\\\setcounter{section}{0}
 \\\usepackage[tiny]{titlesec}
-\\titlespacing\\chapter{0mm}{0mm}{0mm}
-\\titlespacing\\section{0mm}{0mm}{0mm}
-\\DeclareUnicodeCharacter{221E}{\\ensuremath{\\infty}}
-\\DeclareUnicodeCharacter{FFFD}{\\ensuremath{ }}
+\\\titlespacing\\\chapter{0mm}{0mm}{0mm}
+\\\titlespacing\\\section{0mm}{0mm}{0mm}
+\\\DeclareUnicodeCharacter{221E}{\\\ensuremath{\\\infty}}
+\\\DeclareUnicodeCharacter{FFFD}{\\\ensuremath{ }}
 \\\usepackage{fancyhdr}
-\\pagestyle{fancy}
-\\fancyhf{}
-\\fancyfoot[C]{\\thepage}
-\\renewcommand{\\headrulewidth}{0mm}
-\\renewcommand{\\footrulewidth}{0mm}
-\\renewcommand{\\baselinestretch}{0.7}
-\\begin{document}
-\\sf
-\\noindent{\\Large LinksPlatform's Platform.${TRAVIS_REPO_NAME} Class Library}
+\\\pagestyle{fancy}
+\\\fancyhf{}
+\\\fancyfoot[C]{\\\thepage}
+\\\renewcommand{\\\headrulewidth}{0mm}
+\\\renewcommand{\\\footrulewidth}{0mm}
+\\\renewcommand{\\\baselinestretch}{0.7}
+\\\begin{document}
+\\\sf
+\\\noindent{\\\Large LinksPlatform's Platform.${TRAVIS_REPO_NAME} Class Library}
 """
 
 # Remove auto-generated code files
@@ -45,6 +45,6 @@ find ./obj -type f -iname "*.cs" -delete
 find . -type f -iname '*.cs' | sort -b | python format-csharp-files.py
 
 printf """
-\\printindex
-\\end{document}
+\\\printindex
+\\\end{document}
 """
