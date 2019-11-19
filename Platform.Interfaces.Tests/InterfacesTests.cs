c0d290c20f4d63974cba044ab1325c803f534312
Константин Дьяченко <konard@me.com>
2019-11-19 10:26:46 +0300

    Added build test, that is going to be translated to C++.

---
+++ Platform.Interfaces.Tests/InterfacesTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+#pragma warning disable CS0168 // Variable is declared but never used
+
+namespace Platform.Interfaces.Tests
+{
+    public static class InterfacesTests
+    {
+        [Fact]
+        public static void BuildTest()
+        {
+            ICounter<int, int> c1;
+            ICounter<int> c2;
+            ICriterionMatcher<int> cm1;
+            IFactory<int> f1;
+            IProperties<int, int, int> p1;
+            IProperty<int, int> p2;
+            IProvider<int, int> p3;
+            IProvider<int> p4;
+            ISetter<int, int> s1;
+            ISetter<int> s2;
+        }
+    }
+}
