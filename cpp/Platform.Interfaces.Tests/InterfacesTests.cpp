968f254efbc5d9ee72ec7a9c3d340bc87dba1a4e
Константин Дьяченко <konard@me.com>
2019-11-19 10:50:43 +0300

    Added C++ translation for Platform.Interfaces.Tests.

---
+++ cpp/Platform.Interfaces.Tests/InterfacesTests.cpp
@@ -0,0 +1,20 @@
+﻿namespace Platform::Interfaces::Tests
+{
+    TEST_CLASS(Interfaces)
+    {
+    public:
+        TEST_METHOD(BuildTest)
+        {
+            ICounter<int, int> *c1;
+            ICounter<int> *c2;
+            ICriterionMatcher<int> *cm1;
+            IFactory<int> *f1;
+            //IProperties<int, int, int> p1;
+            //IProperty<int, int> p2;
+            IProvider<int, int> *p3;
+            IProvider<int> *p4;
+            ISetter<int, int> *s1;
+            ISetter<int> *s2;
+        }
+    };
+}
