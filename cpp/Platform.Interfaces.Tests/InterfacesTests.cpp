7b4f48b2e8634fd022ad4ee8614ecfcb53f697ac
Konstantin Diachenko <drakonard@gmail.com>
2020-03-05 09:17:46 +0300

    Warnings fix.

--- cpp/Platform.Interfaces.Tests/InterfacesTests.cpp
+++ cpp/Platform.Interfaces.Tests/InterfacesTests.cpp
@@ -4,16 +4,16 @@
     {
         public: TEST_METHOD(BuildTest)
         {
-            ICounter<std::int32_t, std::int32_t>* c1;
-            ICounter<std::int32_t>* c2;
-            ICriterionMatcher<std::int32_t>* cm1;
-            IFactory<std::int32_t>* f1;
-            IProperties<std::int32_t, std::int32_t, std::int32_t>* p1;
-            IProperty<std::int32_t, std::int32_t>* p2;
-            IProvider<std::int32_t, std::int32_t>* p3;
-            IProvider<std::int32_t>* p4;
-            ISetter<std::int32_t, std::int32_t>* s1;
-            ISetter<std::int32_t>* s2;
+            ICounter<std::int32_t, std::int32_t>* c1 = nullptr;
+            ICounter<std::int32_t>* c2 = nullptr;
+            ICriterionMatcher<std::int32_t>* cm1 = nullptr;
+            IFactory<std::int32_t>* f1 = nullptr;
+            IProperties<std::int32_t, std::int32_t, std::int32_t>* p1 = nullptr;
+            IProperty<std::int32_t, std::int32_t>* p2 = nullptr;
+            IProvider<std::int32_t, std::int32_t>* p3 = nullptr;
+            IProvider<std::int32_t>* p4 = nullptr;
+            ISetter<std::int32_t, std::int32_t>* s1 = nullptr;
+            ISetter<std::int32_t>* s2 = nullptr;
         }
     };
 }
