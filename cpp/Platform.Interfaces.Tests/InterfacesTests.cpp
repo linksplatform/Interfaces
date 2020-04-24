c39eb36b0f188539fc1b8abe4ea49b9c2616723e
Konstantin Diachenko <drakonard@gmail.com>
2020-04-24 21:19:29 +0300

    Better styling of pointer in the translation applied.

--- cpp/Platform.Interfaces.Tests/InterfacesTests.cpp
+++ cpp/Platform.Interfaces.Tests/InterfacesTests.cpp
@@ -4,16 +4,16 @@
     {
         public: TEST_METHOD(BuildTest)
         {
-            ICounter<std::int32_t, std::int32_t>* c1 = {};
-            ICounter<std::int32_t>* c2 = {};
-            ICriterionMatcher<std::int32_t>* cm1 = {};
-            IFactory<std::int32_t>* f1 = {};
-            IProperties<std::int32_t, std::int32_t, std::int32_t>* p1 = {};
-            IProperty<std::int32_t, std::int32_t>* p2 = {};
-            IProvider<std::int32_t, std::int32_t>* p3 = {};
-            IProvider<std::int32_t>* p4 = {};
-            ISetter<std::int32_t, std::int32_t>* s1 = {};
-            ISetter<std::int32_t>* s2 = {};
+            ICounter<std::int32_t, std::int32_t> *c1 = {};
+            ICounter<std::int32_t> *c2 = {};
+            ICriterionMatcher<std::int32_t> *cm1 = {};
+            IFactory<std::int32_t> *f1 = {};
+            IProperties<std::int32_t, std::int32_t, std::int32_t> *p1 = {};
+            IProperty<std::int32_t, std::int32_t> *p2 = {};
+            IProvider<std::int32_t, std::int32_t> *p3 = {};
+            IProvider<std::int32_t> *p4 = {};
+            ISetter<std::int32_t, std::int32_t> *s1 = {};
+            ISetter<std::int32_t> *s2 = {};
         }
     };
 }
