f7eab95aced3bc4c95bd78b353f34e63ce609709
Konstantin Diachenko <drakonard@gmail.com>
2020-05-06 20:52:13 +0300

    ICounter code comment simplification.

--- csharp/Platform.Interfaces/ICounter[TResult, TArgument].cs
+++ csharp/Platform.Interfaces/ICounter[TResult, TArgument].cs
@@ -1,16 +1,16 @@
 ﻿namespace Platform.Interfaces
 {
     /// <summary>
-    /// <para>Defines a counter that requires passing an argument to perform a count.</para>
-    /// <para>Определяет счётчик, который требует передачи аргумента для выполнения подсчёта.</para>
+    /// <para>Defines a counter that requires an argument to perform a count.</para>
+    /// <para>Определяет счётчик, которому требуется аргумент для выполнения подсчёта.</para>
     /// </summary>
     /// <typeparam name="TArgument"><para>The argument type.</para><para>Тип аргумента.</para></typeparam>
     /// <typeparam name="TResult"><para>The count result type.</para><para>Тип результата подсчёта.</para></typeparam>
     public interface ICounter<out TResult, in TArgument>
     {
         /// <summary>
-        /// <para>Performs a count that requires passing an argument.</para>
-        /// <para>Выполняет подсчёт, для которого требуется передача аргумента.</para>
+        /// <para>Performs a count.</para>
+        /// <para>Выполняет подсчёт.</para>
         /// </summary>
         /// <param name="argument"><para>The argument.</para><para>Аргумент.</para></param>
         /// <returns><para>The count result.</para><para>Результат подсчёта.</para></returns>
