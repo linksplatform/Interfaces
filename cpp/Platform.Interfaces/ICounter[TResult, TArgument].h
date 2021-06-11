d36ff96d90607c5e941fad18904eec58ec7f767a
uselessgoddess <dodickgod@gmail.com>
2021-06-11 20:17:02 +0300

    Swap to files

--- cpp/Platform.Interfaces/ICounter[TResult, TArgument].h
+++ cpp/Platform.Interfaces/ICounter[TResult, TArgument].h
@@ -1,4 +1,9 @@
 ﻿namespace Platform::Interfaces
 {
-    // SEE ICounter[TResult]
+    template <typename Self, typename TResult, typename... TArgument>
+    concept ICounter = sizeof...(TArgument) <= 1 &&
+                       requires(Self self, TArgument... argument)
+{
+{ self.Count(argument...) } -> std::same_as<TResult>;
+};
 }
--- cpp/Platform.Interfaces/ICounter[TResult].h
+++ cpp/Platform.Interfaces/ICounter[TResult].h
@@ -1,9 +1,4 @@
 ﻿namespace Platform::Interfaces
 {
-    template <typename Self, typename TResult, typename... TArgument>
-    concept ICounter = sizeof...(TArgument) <= 1 &&
-    requires(Self self, TArgument... argument)
-    {
-        { self.Count(argument...) } -> std::same_as<TResult>;
-    };
+    // SEE ICounter[TResult, TArgument]
 }
