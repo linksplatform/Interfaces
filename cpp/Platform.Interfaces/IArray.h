b4a4237a114e7216176bcbfb87fd3b2ffb820003
uselessgoddess <dodickgod@gmail.com>
2021-06-19 17:17:23 +0300

    [C++] 0.1.1 - Moved some interfaces{concepts} from Platform.Collections:   => IEnumerable   => IArray   => IList

---
+++ cpp/Platform.Interfaces/IArray.h
@@ -0,0 +1,33 @@
+#ifndef IARRAY
+#define IARRAY
+
+namespace Platform::Interfaces
+{
+    template<typename Self, typename... Item>
+    concept IArray =
+        IEnumerable<Self> &&
+        sizeof...(Item) <= 1 &&
+    requires(std::tuple<Item...> items)
+    {
+        requires
+        requires
+        {
+            requires sizeof...(Item) == 1;
+
+            requires std::same_as<std::ranges::range_value_t<Self>, decltype(std::get<0>(items))>;
+            requires std::ranges::random_access_range<Self>;
+        }
+        ||
+        requires
+        {
+            requires sizeof...(Item) == 0;
+
+            requires std::ranges::random_access_range<Self>;
+        };
+    };
+
+    template<IArray Self>
+    struct Array : Enumerable<Self> {};
+}
+
+#endif //IARRAY
