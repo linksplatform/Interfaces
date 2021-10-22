d0373252d036e82866a4ac143d3d7ef04a87761b
Mitron57 <85793630+Mitron57@users.noreply.github.com>
2021-10-22 20:41:46 +0400

    Delete ICriterionMatcher.h

--- cpp/Platform.Interfaces/ICriterionMatcher.h
+++
@@ -1,8 +0,0 @@
-﻿namespace Platform::Interfaces
-{
-    template <typename Self, typename TArgument>
-    concept ICriterionMatcher = requires(Self self, TArgument argument)
-    {
-        { self.IsMatched(argument) } -> std::same_as<bool>;
-    };
-}
