1d7a87d1aa9487131863907ef63160a182a66078
Mitron57 <85793630+Mitron57@users.noreply.github.com>
2021-10-22 20:42:31 +0400

    Delete IProperty.h

--- cpp/Platform.Interfaces/IProperty.h
+++
@@ -1,5 +0,0 @@
-﻿namespace Platform::Interfaces
-{
-    template<typename Self, typename TObject, typename TValue>
-    concept IProperty = ISetter<Self, TValue, TObject> && IProvider<Self, TValue, TObject>;
-}
