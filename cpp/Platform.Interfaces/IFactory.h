ba18d17d47c7a4bbc723b7f16ff342e471bb8b9e
Mitron57 <85793630+Mitron57@users.noreply.github.com>
2021-10-22 20:42:12 +0400

    Delete IFactory.h

--- cpp/Platform.Interfaces/IFactory.h
+++
@@ -1,8 +0,0 @@
-﻿namespace Platform::Interfaces
-{
-    template <typename Self, typename TProduct>
-    concept IFactory = requires(Self self)
-    {
-        { self.Create() } -> std::same_as<TProduct>;
-    };
-}
