8cfe769bf56c29d674621cf871b65651be30def2
Mitron57 <85793630+Mitron57@users.noreply.github.com>
2021-10-25 13:49:31 +0400

    Class instead of struct

--- cpp/Platform.Interfaces/Extended.h
+++ cpp/Platform.Interfaces/Extended.h
@@ -6,7 +6,7 @@
 namespace Platform::Interfaces
 {
     template<typename TExtendable, template<typename> typename TFirstExtender, template<typename> typename ...TExtenders>
-    struct Extended : public EXTENDED_BASE_TYPE(Extended, ExtendedBase, TExtendable, TFirstExtender, TExtenders)
+    class Extended : public EXTENDED_BASE_TYPE(Extended, ExtendedBase, TExtendable, TFirstExtender, TExtenders)
     {
     };
 }
