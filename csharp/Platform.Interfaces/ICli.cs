5ee1a76af31b98d3b107643b289eb795a998778d
FreePhoenix888 <freephoenix888@gmail.com>
2022-02-11 18:16:15 +0600

    Add XML Docs

--- csharp/Platform.Interfaces/ICli.cs
+++ csharp/Platform.Interfaces/ICli.cs
@@ -1,6 +1,19 @@
 namespace Platform.Interfaces;
 
+/// <summary>
+/// <para>Defines command line interfaces.</para>
+/// <para>Определяет интерфейс коммандной строки.</para>
+/// </summary>
+/// <typeparam name="TLinkAddress"></typeparam>
 public interface ICli<TLinkAddress>
 {
+    /// <summary>
+    /// <para>Runs command line interface.</para>
+    /// <para>Запускает интерфейс коммандной строки.</para>
+    /// </summary>
+    /// <param name="args">
+    /// <para>Command line arguments.</para>
+    /// <para>Аргументы коммандной строки/</para>
+    /// </param>
     void Run(params string[] args);
 }
