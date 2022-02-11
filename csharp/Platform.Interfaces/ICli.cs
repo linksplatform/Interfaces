b43fb0ecdcdf848abd7de383102ad6d7ef6de2a4
FreePhoenix888 <66206278+FreePhoenix888@users.noreply.github.com>
2022-02-11 19:14:45 +0600

    Update ICli

--- csharp/Platform.Interfaces/ICli.cs
+++ csharp/Platform.Interfaces/ICli.cs
@@ -1,21 +1,21 @@
 namespace Platform.Interfaces;
 
 /// <summary>
-/// <para>Defines command line interfaces.</para>
+/// <para>Defines command line interfaces for command that interacts with an operational system.</para>
 /// <para>Определяет интерфейс командной строки, для команды взаимодействующей с операционной системой.</para>
 /// </summary>
 public interface ICli
 {
     /// <summary>
-    /// <para>Runs commands.</para>
+    /// <para>Runs command.</para>
     /// <para>Запускает команду.</para>
     /// </summary>
     /// <param name="args">
-    /// <para>Command line arguments.</para>
+    /// <para>Arguments for command.</para>
     /// <para>Аргументы для команды.</para>
     /// </param>
     /// <returns>
-    /// <para>Returns an exit code.</para>
+    /// <para>Returns an command exit code.</para>
     /// <para>Возвращает код выхода команды.</para>
     /// </returns>
     int Run(params string[] args);
