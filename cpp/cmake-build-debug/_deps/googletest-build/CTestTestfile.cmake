1b2103d022477216bef7a14beabe6100974bad64
FreePhoenix888 <freephoenix888@gmail.com>
2022-02-14 14:11:56 +0600

    Revert "Add .net6 support"
    This reverts commit c678bcf9985a69dba2008e4d46f57a10748c674e.

--- cpp/cmake-build-debug/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/freephoenix888/Documents/Programming/LinksPlatform/Interfaces/cpp/cmake-build-debug/_deps/googletest-src
-# Build directory: /home/freephoenix888/Documents/Programming/LinksPlatform/Interfaces/cpp/cmake-build-debug/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
