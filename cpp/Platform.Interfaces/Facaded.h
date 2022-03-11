97abb86e0c5614f66c6623a280397e1f1097a7ab
Konstantin Dyachenko <drakonard@gmail.com>
2022-03-11 19:14:51 +0300

    Formatting repair.

--- cpp/Platform.Interfaces/Facaded.h
+++ cpp/Platform.Interfaces/Facaded.h
@@ -4,11 +4,11 @@
 
 namespace Platform::Interfaces
 {
-  template<typename TExtendable, template<typename, typename> typename TFirstExtender, template<typename, typename> typename ...TExtenders>
-  struct Facaded : public FacadedIntermediate<Facaded<TExtendable, TFirstExtender, TExtenders...>, TExtendable, TFirstExtender, TExtenders...>
-  {
-    using base = FacadedIntermediate<Facaded<TExtendable, TFirstExtender, TExtenders...>, TExtendable, TFirstExtender, TExtenders...>;
+    template<typename TExtendable, template<typename, typename> typename TFirstExtender, template<typename, typename> typename ...TExtenders>
+    struct Facaded : public FacadedIntermediate<Facaded<TExtendable, TFirstExtender, TExtenders...>, TExtendable, TFirstExtender, TExtenders...>
+    {
+        using base = FacadedIntermediate<Facaded<TExtendable, TFirstExtender, TExtenders...>, TExtendable, TFirstExtender, TExtenders...>;
 
-    USE_ALL_BASE_CONSTRUCTORS(Facaded, base)
-  };
+        USE_ALL_BASE_CONSTRUCTORS(Facaded, base)
+    };
 }
--- cpp/Platform.Interfaces/FacadedIntermediate.h
+++ cpp/Platform.Interfaces/FacadedIntermediate.h
@@ -4,11 +4,11 @@
 
 namespace Platform::Interfaces
 {
-  template<typename TFacaded, typename TExtendable, template<typename, typename> typename TFirstExtender, template<typename, typename> typename ...TExtenders>
-  struct FacadedIntermediate : public FACADED_BASE_TYPE(FacadedIntermediate, TFacaded, TExtendable, TFirstExtender, TExtenders)
-  {
-      using base = FACADED_BASE_TYPE(FacadedIntermediate, TFacaded, TExtendable, TFirstExtender, TExtenders);
+    template<typename TFacaded, typename TExtendable, template<typename, typename> typename TFirstExtender, template<typename, typename> typename ...TExtenders>
+    struct FacadedIntermediate : public FACADED_BASE_TYPE(FacadedIntermediate, TFacaded, TExtendable, TFirstExtender, TExtenders)
+    {
+        using base = FACADED_BASE_TYPE(FacadedIntermediate, TFacaded, TExtendable, TFirstExtender, TExtenders);
 
-      USE_ALL_BASE_CONSTRUCTORS(FacadedIntermediate, base)
-  };
+        USE_ALL_BASE_CONSTRUCTORS(FacadedIntermediate, base)
+    };
 }
