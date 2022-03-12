54da309ae54d32187bbb029e6421e35b962e96a2
Konstantin Dyachenko <drakonard@gmail.com>
2022-03-12 12:20:23 +0300

    Decorated repair.

--- cpp/Platform.Interfaces/Decorated.h
+++ cpp/Platform.Interfaces/Decorated.h
@@ -5,9 +5,9 @@
 namespace Platform::Interfaces
 {
     template<typename TDecorated, template<typename, typename> typename TFirstDecorator, template<typename, typename> typename ...TDecorators>
-    struct Decorated : public DecoratedIntermediate<Decorated<TDecorated, TFirstDecorator, TDecorators...>, TDecorated, TFirstDecorator, TDecorators...>
+    struct Decorated : public DecoratedBase<Decorated<TDecorated, TFirstDecorator, TDecorators...>, TDecorated, TFirstDecorator, TDecorators...>
     {
-        using base = DecoratedIntermediate<Decorated<TDecorated, TFirstDecorator, TDecorators...>, TDecorated, TFirstDecorator, TDecorators...>;
+        using base = DecoratedBase<Decorated<TDecorated, TFirstDecorator, TDecorators...>, TDecorated, TFirstDecorator, TDecorators...>;
 
         USE_ALL_BASE_CONSTRUCTORS(Decorated, base)
     };
