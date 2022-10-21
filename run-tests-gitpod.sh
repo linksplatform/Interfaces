3497d6278149ae70c6fa697dc91f37927ff3fff9
Daulet Nassipkali <uralskdev@gmail.com>
2022-10-21 21:26:22 +0500

    Update run-tests-gitpod.sh

--- run-tests-gitpod.sh
+++ run-tests-gitpod.sh
@@ -6,7 +6,7 @@ cmake_flags="-DCMAKE_BUILD_TYPE=Release -DLINKS_PLATFORM_TESTS=ON"
 cmake_build_dir="build"
 mkdir -p $cmake_build_dir && cd $cmake_build_dir
 conan install .. -pr=linksplatform --build=missing
-cmake .. $cmake_flags
+cmake .. "$cmake_flags"
 cmake --build .
 scan-build cmake --build .
 binaries=bin/*
