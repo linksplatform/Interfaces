a5948eec3062f6cc1844a6bad9a9fea9ffbcc5a6
FreePhoenix888 <freephoenix888@gmail.com>
2022-10-27 21:52:16 +0600

    Fix type: TDecorated -> TFacade

--- cpp/Platform.Interfaces/DecoratorBase.h
+++ cpp/Platform.Interfaces/DecoratorBase.h
@@ -10,6 +10,6 @@ namespace Platform::Interfaces
         USE_ALL_BASE_CONSTRUCTORS(DecoratorBase, TDecorated)
 
         THIS_REFERENCE_WRAPPER_METHODS(decorated, TDecorated)
-        THIS_REFERENCE_WRAPPER_METHODS(facade, TDecorated)
+        THIS_REFERENCE_WRAPPER_METHODS(facade, TFacade)
     };
 }
