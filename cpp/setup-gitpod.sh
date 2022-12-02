e8f10cbf6742ef0bcd253c878a9d1f45f1aa9dab
FreePhoenix888 <freephoenix888@gmail.com>
2022-12-02 16:17:32 +0600

    Update setup-gitpod.sh

--- cpp/setup-gitpod.sh
+++ cpp/setup-gitpod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
   
 cd ..
-
+ 
 sudo apt install -y g++-10 cmake
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 100 --slave /usr/bin/g++ g++ /usr/bin/g++-10 --slave /usr/bin/gcov gcov /usr/bin/gcov-10
 
